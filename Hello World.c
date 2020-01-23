200395532b953679b96998ad8cbd2cccecfc0c99
adarshrajpkd <60054806+adarshrajpkd@users.noreply.github.com>
2020-01-23 15:05:43 +0530

    Add files via upload

---
+++ Hello World.c
@@ -0,0 +1,9 @@
+int main()
+{
+	printf("HELLO WORLD\n\n");
+	printf("HELLO\nWORLD\n\n");
+	printf("H\nE\nL\nL\nO\n\nW\nO\nR\nL\nD\n\n");
+	printf("HELLO\tWORLD\n\n");
+	printf("H\tE\tL\tL\tO\n\t\t\t\tW\tO\tR\tL\tD");
+	getch();
+}
---
+++ Sum.c
@@ -0,0 +1,9 @@
+int main()
+{
+	int a,b,sum;
+	printf("Enter Two Numbers To Find Their Sum : ");
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	printf("\n The Sum Of %d and %d is : %d",a,b,sum);
+	getch();
+}
